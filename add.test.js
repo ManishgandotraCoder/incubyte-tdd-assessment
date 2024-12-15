55274049bae659cea01174e41b5d73b4f242ce4c
manish <manishgandotra@icloud.com>
2024-12-15 11:09:27 +0530

    test: add test case for summing multiple numbers

--- add.test.js
+++ add.test.js
@@ -5,11 +5,16 @@ describe("String Calculator", () => {
   it("should return 0 for an empty string", () => {
     expect(add("")).to.equal(0);
   });
+
   it("should return the same number for a single number", () => {
     expect(add("1")).to.equal(1);
   });
 
   it("should return the sum for two numbers", () => {
     expect(add("1,5")).to.equal(6);
+  });
+
+  it("should return the sum for multiple numbers", () => {
+    expect(add("1,2,3")).to.equal(6);
   });
 });
