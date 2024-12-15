b4eb2e8e8b92b10033afe5efb96d7dae0857e47a
manish <manishgandotra@icloud.com>
2024-12-15 23:03:16 +0530

    feat: throw error for negative numbers

--- add.js
+++ add.js
@@ -4,12 +4,17 @@ export default function add(numbers) {
   let delimiter = ",|\n";
   if (numbers.startsWith("//")) {
     const delimiterLine = numbers.split("\n")[0];
-    delimiter = delimiterLine.substring(2); // Extract the custom delimiter
-    numbers = numbers.substring(delimiterLine.length + 1); // Remove delimiter line
+    delimiter = delimiterLine.substring(2);
+    numbers = numbers.substring(delimiterLine.length + 1);
   }
 
   const numberArray = numbers
     .split(new RegExp(`[${delimiter}]`))
     .map((num) => parseInt(num, 10));
+
+  const negatives = numberArray.filter((n) => n < 0);
+  if (negatives.length)
+    throw new Error(`negative numbers not allowed ${negatives.join(", ")}`);
+
   return numberArray.reduce((total, num) => total + num, 0);
 }
