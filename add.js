d7e2f4514947b96d0b7f6588810462c1abfdb1cd
manish <manishgandotra@icloud.com>
2024-12-15 23:01:01 +0530

    feat: implement logic to handle newlines as delimiters

--- add.js
+++ add.js
@@ -1,5 +1,6 @@
 export default function add(numbers) {
   if (numbers === "") return 0;
-  const numberArray = numbers.split(",").map((num) => parseInt(num, 10));
+
+  const numberArray = numbers.split(/,|\n/).map((num) => parseInt(num, 10)); // Support commas and newlines
   return numberArray.reduce((total, num) => total + num, 0);
 }
