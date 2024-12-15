25ccdcf8b9018c389f9f1906020b642ae5d49d43
manish <manishgandotra@icloud.com>
2024-12-15 23:02:17 +0530

    feat: implement logic to handle custom delimiters

--- add.js
+++ add.js
@@ -1,6 +1,15 @@
 export default function add(numbers) {
   if (numbers === "") return 0;
 
-  const numberArray = numbers.split(/,|\n/).map((num) => parseInt(num, 10)); // Support commas and newlines
+  let delimiter = ",|\n";
+  if (numbers.startsWith("//")) {
+    const delimiterLine = numbers.split("\n")[0];
+    delimiter = delimiterLine.substring(2); // Extract the custom delimiter
+    numbers = numbers.substring(delimiterLine.length + 1); // Remove delimiter line
+  }
+
+  const numberArray = numbers
+    .split(new RegExp(`[${delimiter}]`))
+    .map((num) => parseInt(num, 10));
   return numberArray.reduce((total, num) => total + num, 0);
 }
