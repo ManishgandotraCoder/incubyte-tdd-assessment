933657d293bdd64774782fa26ee281f152648731
manish <manishgandotra@icloud.com>
2024-12-15 10:56:19 +0530

    test: add test case for empty string returning 0

--- add.test.js
+++ add.test.js
@@ -1,4 +1,8 @@
 import { expect } from "chai";
 import add from "./add.js";
 
-describe("String Calculator", () => {});
+describe("String Calculator", () => {
+  it("should return 0 for an empty string", () => {
+    expect(add("")).to.equal(0);
+  });
+});
